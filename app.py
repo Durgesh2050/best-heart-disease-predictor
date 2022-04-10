d9d638f17a432da7c88dc18e77a1efa5867eec98
Durgesh Kumar <78895337+Durgesh2050@users.noreply.github.com>
2022-04-11 00:29:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2022 Neelkant Newra
+Copyright (c) 2022 Best Heart Disease predication
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
